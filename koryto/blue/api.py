906ac34196515654c95fcfcbc215a1535c1a2c46
Janusz Lewandowski <lew21@enves.pl>
2012-06-07 22:40:41 +0200

    Lazy is now less lazy.

--- koryto/blue/api.py
+++ koryto/blue/api.py
@@ -21,8 +21,14 @@ class lazy(object):
 
 	def __init__(self, load):
 		self.load = load
+		try:
+			self.__doc__ = load.__doc__
+		except:
+			pass
 
 	def __get__(self, obj, type=None):
+		if obj is None:
+			return self
 		value = self.load(obj)
 		setattr(obj, self.load.__name__, value)
 		return value
